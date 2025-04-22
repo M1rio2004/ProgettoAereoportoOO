af8cbeac23d35bb32b06192e421e3dde9773f45a
Alessio470 <nazzaroalessio@gmail.com>
2025-04-22 17:17:44 +0200

    implementazione main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,29 @@
-public class Main {
-    public static void main(String[] args) {
+package model;
 
-        //System.out.printf("Hello and welcome!");///////
-        //test
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.Duration;
+
+public class MainTest {
+    public static void main(String[] args) {
+        VoloPartenzaDaNapoli volo1 = new VoloPartenzaDaNapoli(
+                "NAP123", "ITA Airways",
+                LocalDate.now(), LocalTime.of(14, 30),
+                Duration.ofMinutes(0), StatoVolo.PROGRAMMATO,
+                "Roma Fiumicino"
+        );
+
+        Gate gate5 = new Gate(5);
+        UtenteAmministratore admin = new UtenteAmministratore("admin", "password");
+        admin.modificaGate(volo1, gate5);
+
+        UtenteGenerico utente = new UtenteGenerico("utente1", "password1");
+        Prenotazione prenotazione = new Prenotazione(
+                "TCK123", "RSSMRA00A01H501Z", "Mario", "Rossi",
+                "12A", StatoPrenotazione.CONFERMATA, volo1
+        );
+
+        utente.prenotaVolo(volo1, prenotazione);
+        utente.visualizzaPrenotazioni();
     }
 }
